35c97a132b21381040496e76cff47c9c4e4bf820
prince <princeudum1@gmail.com>
2023-07-20 13:39:34 +0100

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Gallery from "./Gallery";
+import SearchForm from "./SearchForm";
+import ToggleTheme from "./ToggleTheme";
+
+const App = () => {
+  return (
+    <main>
+      <ToggleTheme />
+      <SearchForm />
+      <Gallery />
+    </main>
+  );
+};
+export default App;
